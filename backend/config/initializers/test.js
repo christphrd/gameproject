a89ecc7febbe5fcecebb1bb49b9012c3bfe5bf03
izzee <ichabon@me.com>
2018-03-28 16:15:18 -0400

    refactoring high-scores

---
+++ backend/config/initializers/test.js
@@ -0,0 +1,22 @@
+function postData(){
+  return fetch('http://localhost:3000/scores',{
+      method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          points: 10235,
+          user_initial: "CD",
+          level_id: 3
+        })
+    }
+  )
+  .then(res => res.json())
+  .then(console.log)
+}
+function checkData(){
+    return fetch('http://localhost:3000/levels/3')
+    .then(res => res.json())
+    .then(console.log)
+  }
