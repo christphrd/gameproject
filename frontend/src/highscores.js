9a4efc5125f5d4fe56b3ef248d8404e1ce0a5503
izzee <ichabon@me.com>
2018-03-28 17:03:52 -0400

    highscores is table

--- frontend/src/highscores.js
+++ frontend/src/highscores.js
@@ -10,14 +10,21 @@ function displayScores(data) {
   document.getElementById('highscores').children[2].addEventListener('click', resetGame)
   document.getElementById('game-window').style.display = 'none'
 
-  let highScoreList = document.getElementById('high-score-list')
-  highScoreList.innerText = " "
+  let highScoreTable = document.getElementById('high-score-table')
+  highScoreTable.innerHTML = "<tr><th>Name</th><th>Score</th></tr>"
 
   let topTenScores = data.scores.slice(0,10)
 
   for(let i=0; i< topTenScores.length; i++){
-    let scoreElement= document.createElement('li')
-    scoreElement.innerText = `${topTenScores[i].user_initial}: ${topTenScores[i].points}`
-    highScoreList.append(scoreElement)
+    let row = document.createElement('tr')
+    let username = document.createElement('td')
+    username.innerText = topTenScores[i].user_initial
+    row.append(username)
+
+    let score = document.createElement('td')
+    score.innerText =topTenScores[i].points
+    row.append(score)
+
+    highScoreTable.append(row)
   }
 }
