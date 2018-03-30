21ddb021c665041aa2d9dbae31b736cea3963401
alin684 <al684@cornell.edu>
2018-03-30 11:40:53 -0400

    random birds

--- frontend/src/bird.js
+++ frontend/src/bird.js
@@ -1,17 +1,23 @@
 
 class Bird {
-  constructor(speed = random(-5,5), scalar = random(10,70), angle = random(2,5)){
+  constructor(speed = random(-5,5), scalar = random(10,70), angle = random(2,5), randomBird = floor(random(0,2))){
     this.x = random(width)
     this.y = random(40, 100)
     this.birdSpeed = speed
     this.scalar = scalar
     this.angle = angle
     this.origAngle = angle
-    this.image = loadGif("img/bird.gif")
+    this.image1 = loadGif("img/bird.gif")
+    this.image2 = loadGif("img/bird2.gif")
+    this.randomBird = randomBird
   }
 
   show(){
-    image(this.image, this.x, this.y + (this.scalar * sin(this.angleToRadians())), 90, 50);
+    if(this.randomBird===0){
+      image(this.image1, this.x, this.y + (this.scalar * sin(this.angleToRadians())), 90, 50);
+    }else if(this.randomBird===1){
+      image(this.image2, this.x, this.y + (this.scalar * sin(this.angleToRadians())), 90, 50);
+    }
     if(this.x>width+50 && this.x >1){
       this.x = -50
     } else if(this.x<-50 && this.x < -1) {
